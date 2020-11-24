b84c11c6174971a7726969dc25e440f779390a53
John Carlo Cunanan <codingjohn@outlook.com>
2020-11-24 21:30:12 +0800

    done with homepage component

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./main.css";
+
 const Main = () => {
   return (
     <>
       <main className="main">
         <TopHalf />
-        <Button name="Create Account" />
+        <BottomHalf />
+        <Footer />
       </main>
     </>
   );
@@ -24,12 +26,30 @@ const TopHalf = () => {
   );
 };
 
+const BottomHalf = () => {
+  return (
+    <div className="bottom-half container">
+      <div className="row">
+        <div className="col-12 button-wrapper">
+          <Button name="Create Account" />
+          <Button name="Login" />
+          <Button name="About" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Button = ({ name }) => {
   return (
     <a href="#">
       <button className="button">{name}</button>
     </a>
   );
+};
+
+const Footer = () => {
+  return <footer>Copyright &#169; 2020. All rights reserved.</footer>;
 };
 
 export default Main;
