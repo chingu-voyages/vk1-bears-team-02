6db50e6045d7a4c41356bd7ad62e3ee25b8c08ff
John Carlo Cunanan <codingjohn@outlook.com>
2020-11-25 11:47:46 +0800

    minor changes
    just fixed the background on homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./Components/Main/Main";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Main}></Route>
+        <Route exact path="/Login" component={Login}></Route>
       </Switch>
     </Router>
   );
