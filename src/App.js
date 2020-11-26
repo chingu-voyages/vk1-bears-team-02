afbd3cb1999956d4bbb8c124e0493be07bce87fa
John Carlo Cunanan <codingjohn@outlook.com>
2020-11-26 13:59:06 +0800

    made footer link dynamic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./Components/Main/Main";
 import { Login } from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Main}></Route>
         <Route exact path="/Login" component={Login}></Route>
+        <Route exact path="/Register" component={Register}></Route>
       </Switch>
     </Router>
   );
