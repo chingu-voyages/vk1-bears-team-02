7006ac1022647e68cc23f086d64bb4bc806ae32e
John Carlo Cunanan <codingjohn@outlook.com>
2020-11-26 20:56:57 +0800

    add fake authentication

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Main from "./Components/Main/Main";
 import { Login } from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Private from "./Components/Private";
 
 const App = () => {
+  const [authenticated, setAuthenticated] = useState(true);
+  const PublicRoute = ({ component: Component, authenticated, ...rest }) => {
+    return (
+      <Route
+        {...rest}
+        component={(props) =>
+          authenticated ? <Redirect to="/private" /> : <Component {...props} />
+        }
+      />
+    );
+  };
+  const PrivateRoute = ({ component: Component, authenticated, ...rest }) => {
+    return (
+      <Route
+        {...rest}
+        component={(props) =>
+          authenticated ? <Component {...props} /> : <Redirect to="/login" />
+        }
+      />
+    );
+  };
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Main}></Route>
-        <Route exact path="/Login" component={Login}></Route>
-        <Route exact path="/Register" component={Register}></Route>
+        <PublicRoute
+          exact
+          path="/"
+          component={Main}
+          authenticated={authenticated}
+        ></PublicRoute>
+        <PublicRoute
+          exact
+          path="/Login"
+          component={Login}
+          authenticated={authenticated}
+        ></PublicRoute>
+        <PublicRoute
+          exact
+          path="/Register"
+          component={Register}
+          authenticated={authenticated}
+        ></PublicRoute>
+        <PrivateRoute
+          exact
+          path="/Private"
+          authenticated={authenticated}
+          component={Private}
+        ></PrivateRoute>
       </Switch>
     </Router>
   );
 };
+
 export default App;
