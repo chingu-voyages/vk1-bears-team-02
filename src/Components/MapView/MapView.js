e0ab6311297274bff582c2289a13e2c4ece2109f
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-11-30 02:58:13 +0800

    created new version of map view with lat and long indicator

--- src/Components/MapView/MapView.js
+++ src/Components/MapView/MapView.js
@@ -16,21 +16,20 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 const styles = {
 	width: "100vw",
 	height: "calc(100vh - 80px)",
+	// height: "100vh",
 	// position: "absolute",
 };
 const markerHeight = 50,
 	markerRadius = 10,
 	linearOffset = 25;
+
 const popupOffsets = {
 	top: [0, 0],
-	"top-left": [0, 0],
-	"top-right": [0, 0],
+	topLeft: [0, 0],
+	topRight: [0, 0],
 	bottom: [0, -markerHeight],
-	"bottom-left": [
-		linearOffset,
-		(markerHeight - markerRadius + linearOffset) * -1,
-	],
-	"bottom-right": [
+	bottomLeft: [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+	bottomRight: [
 		-linearOffset,
 		(markerHeight - markerRadius + linearOffset) * -1,
 	],
@@ -40,43 +39,47 @@ const popupOffsets = {
 
 const MapView = () => {
 	const [map, setMap] = useState(null);
-	const [coordinates, setCoordinates] = useState([
-		{
-			lng: 121.0244, //n to s
-			lat: 14.5547, //e to w
-			description: "Fire at this location",
-		},
-		{
-			lng: 121.0437, //n to s
-			lat: 14.676, //e to w
-			description: "Fire at this location",
-		},
+	// const [coordinates, setCoordinates] = useState([
+	// 	{
+	// 		lng: 121.0244, //n to s
+	// 		lat: 14.5547, //e to w
+	// 		description: "Fire at this location",
+	// 	},
+	// 	{
+	// 		lng: 121.0437, //n to s
+	// 		lat: 14.676, //e to w
+	// 		description: "Fire at this location",
+	// 	},
 
-		{
-			lng: 120.984222, //n to s
-			lat: 14.599512, //e to w
-			description: "Fire at this location",
-		},
-		{
-			lng: 120.81604, //n to s
-			lat: 14.852739, //e to w
-			description: "Earthquake at this location",
-		},
-		{
-			lng: 120.608589, //n to s
-			lat: 15.80002, //e to w
-			description: "flashflood at this location",
-		},
-		{
-			lng: 121.050865, //n to s
-			lat: 14.517618, //e to w
-			description: "Hail Storm at this location",
-		},
-	]);
+	// 	{
+	// 		lng: 120.984222, //n to s
+	// 		lat: 14.599512, //e to w
+	// 		description: "Fire at this location",
+	// 	},
+	// 	{
+	// 		lng: 120.81604, //n to s
+	// 		lat: 14.852739, //e to w
+	// 		description: "Earthquake at this location",
+	// 	},
+	// 	{
+	// 		lng: 120.608589, //n to s
+	// 		lat: 15.80002, //e to w
+	// 		description: "flashflood at this location",
+	// 	},
+	// 	{
+	// 		lng: 121.050865, //n to s
+	// 		lat: 14.517618, //e to w
+	// 		description: "Hail Storm at this location",
+	// 	},
+	// ]);
 	const [initialView, setInitialView] = useState({
 		lng: 120.9842,
 		lat: 14.5995,
 	});
+
+	const [zoom, setZoom] = useState(6);
+
+	const [tiltAngle, setTiltAngle] = useState(1);
 
 	const [geotrigger, setGeotrigger] = useState(false);
 
@@ -88,12 +91,12 @@ const MapView = () => {
 				container: mapContainer.current,
 				style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
 				center: [initialView.lng, initialView.lat],
-				zoom: 6,
-				pitch: 45,
+				zoom: zoom,
+				pitch: tiltAngle,
 			});
 
 			// Initialize the geolocate control.
-			var geolocate = new mapboxgl.GeolocateControl({
+			let geolocate = new mapboxgl.GeolocateControl({
 				positionOptions: {
 					enableHighAccuracy: true,
 				},
@@ -105,29 +108,73 @@ const MapView = () => {
 			map.on("load", () => {
 				setMap(map);
 				// geolocate.trigger();
+
 				map.resize();
 			});
 
-			axios.get(`http://localhost:5000/coordinates`).then((res) => {
-				const data = res.data;
-				//   this.setState({ persons });
-				const coordinate_data = data.data;
-				console.log(coordinate_data);
-
-				coordinate_data.forEach((data) => {
-					// console.log(data.properties.message);
-					new mapboxgl.Marker().setLngLat(data.geometry.coordinates).addTo(map);
-					new mapboxgl.Popup({
-						offset: popupOffsets,
-						className: "popover-style",
-					})
-						.setLngLat(data.geometry.coordinates)
-						//.setHTML(<PopOver message="ivan" />)
-						.setHTML(`<p>${data.properties.message}</p>`)
-						.setMaxWidth("320px")
-						.addTo(map);
+			map.on("move", () => {
+				setInitialView({
+					lng: map.getCenter().lng.toFixed(4),
+					lat: map.getCenter().lat.toFixed(4),
+					// zoom: map.getZoom().toFixed(4),
+					// pitch: initialView.pitch,
 				});
+
+				setZoom(map.getZoom().toFixed(4));
+
+				if (zoom > 7) {
+					alert("s");
+					console.log(`type of ${typeof zoom}`);
+				}
 			});
+
+			// axios.get(`http://localhost:5000/coordinates`).then((res) => {
+			// 	const data = res.data;
+			// 	//   this.setState({ persons });
+			// 	const coordinate_data = data.data;
+			// 	console.log(coordinate_data);
+
+			// 	coordinate_data.forEach((data) => {
+			// 		// console.log(data.properties.message);
+			// 		new mapboxgl.Marker().setLngLat(data.geometry.coordinates).addTo(map);
+			// 		new mapboxgl.Popup({
+			// 			offset: popupOffsets,
+			// 			className: "popover-style",
+			// 		})
+			// 			.setLngLat(data.geometry.coordinates)
+			// 			//.setHTML(<PopOver message="ivan" />)
+			// 			.setHTML(`<p>${data.properties.message}</p>`)
+			// 			.setMaxWidth("320px")
+			// 			.addTo(map);
+			// 	});
+			// });
+
+			const getAllCoordinates = async () => {
+				try {
+					console.log("call this");
+					const data = await axios.get(`http://localhost:5000/coordinates`);
+					const coordinate_data = data.data.data;
+					console.log(coordinate_data);
+
+					coordinate_data.forEach((data) => {
+						// console.log(data.properties.message);
+						new mapboxgl.Marker()
+							.setLngLat(data.geometry.coordinates)
+							.addTo(map);
+						new mapboxgl.Popup({
+							offset: popupOffsets,
+							className: "popover-style",
+						})
+							.setLngLat(data.geometry.coordinates)
+							//.setHTML(<PopOver message="ivan" />)
+							.setHTML(`<p>${data.properties.message}</p>`)
+							.setMaxWidth("320px")
+							.addTo(map);
+					});
+				} catch (error) {
+					console.log(error);
+				}
+			};
 
 			// coordinates.forEach((data) => {
 			// 	new mapboxgl.Marker().setLngLat([data.lng, data.lat]).addTo(map);
@@ -137,22 +184,21 @@ const MapView = () => {
 			// 		.setMaxWidth("500px")
 			// 		.addTo(map);
 			// });
+
+			getAllCoordinates();
 		};
 
 		if (!map) initializeMap({ setMap, mapContainer });
 	}, [map]);
 
 	return (
-		<>
+		<div className="map-container">
 			<div ref={(element) => (mapContainer.current = element)} style={styles} />
-			<button
-				onClick={() => {
-					setGeotrigger(!geotrigger);
-					console.log(geotrigger);
-				}}>
-				Trigger
-			</button>
-		</>
+			<p>
+				Longitude: {initialView.lng} | Latitude: {initialView.lat} | Tilt angle:
+				{tiltAngle} | Zoom : {zoom}
+			</p>
+		</div>
 	);
 };
 
