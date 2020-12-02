6fa1e853e4f5b6931a30a6fe462e1fc56582bf5f
John Carlo Cunanan <codingjohn@outlook.com>
2020-12-02 14:19:24 +0800

    fix responsiveness

--- src/Components/user/HelpButton/HelpButton.js
+++ src/Components/user/HelpButton/HelpButton.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./helpButton.css";
 
-const HelpButton = () => {
+const HelpButton = ({ logo }) => {
+  const [isSent, setStatus] = useState(false);
   const handleClick = () => {
-    console.log("clicked");
+    alert("Your distress message has been sent.");
   };
   return (
     <>
       <div className="help-button-wrapper" onClick={handleClick}>
         <div className="help-button d-flex justify-content-center align-items-center flex-column">
+          {logo}
           <h2>Click to get help!</h2>
         </div>
+        <div class="help-button-outline"></div>
       </div>
     </>
   );
