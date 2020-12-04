bdee016020ac006ce2c09713b06ed0d8254d9a92
John Carlo Cunanan <codingjohn@outlook.com>
2020-12-04 22:35:06 +0800

    fix user nav styles

--- src/Components/admin/Nav/Nav.js
+++ src/Components/admin/Nav/Nav.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-import "./nav.css";
-
 function Nav({ status, setStatus }) {
   const handleClick = (e) => {
     e.preventDefault();
@@ -10,7 +8,15 @@ function Nav({ status, setStatus }) {
   };
   return (
     <>
-      <nav className="user-navbar">
+      <nav
+        className="user-navbar"
+        style={{
+          justifyContent: "flex-end",
+          height: "7%",
+          minHeight: "50px",
+          boxShadow: "0px 12px 10px -11px rgba(0, 0, 0, 0.75)",
+        }}
+      >
         <a href="/" onClick={handleClick}>
           <svg
             className="menu-button p-1"
@@ -18,6 +24,10 @@ function Nav({ status, setStatus }) {
             viewBox="0 -53 384 384"
             width="27px"
             xmlns="http://www.w3.org/2000/svg"
+            style={{
+              border: "1px solid var(--primary-white)",
+              borderRadius: "2px",
+            }}
           >
             <path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
             <path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
