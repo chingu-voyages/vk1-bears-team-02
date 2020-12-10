72ee2606d9beae2304ed339567d575d19d8e99cb
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-12-10 20:26:02 +0800

    add functionality to response button in admin dashboard map view

--- src/Components/MapView/PopOver.js
+++ src/Components/MapView/PopOver.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Badge } from "react-bootstrap";
+import axios from "axios";
 
 export default function PopOver(props) {
-	const { feature, datas } = props;
+	const { datas } = props;
 	const { properties, geometry } = datas;
 	const { coordinates } = geometry;
 	const [lng, lat] = coordinates;
@@ -10,10 +11,59 @@ export default function PopOver(props) {
 
 	console.log(`data: ${geometry.coordinates}`);
 
+	const respond = (property) => {
+		alert(property._id);
+		console.log(property);
+
+		const sendResponse = async () => {
+			try {
+				let new_status = "";
+				if (property.status === "sent") {
+					new_status = "acknowledge";
+				} else if (property.status === "acknowledge") {
+					new_status = "resolved";
+				}
+
+				const data = {
+					status: new_status,
+				};
+				const response = await axios.put(
+					`http://localhost:5000/map-data/${property._id}`,
+					data
+				);
+				console.log(response);
+				window.location.reload();
+				// .then(function (response) {
+				// 	console.log(response);
+				// 	window.location.reload();
+				// })
+				// .catch(function (error) {
+				// 	console.log(error);
+				// });
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		sendResponse();
+	};
+
 	return (
 		<>
 			<div>
 				<h3>{properties.title}</h3>
+				<p>
+					<Badge
+						variant={
+							datas.status === `sent`
+								? `danger`
+								: datas.status === `acknowledge`
+								? `warning`
+								: `success`
+						}>
+						{datas.status}
+					</Badge>
+				</p>
 				<p>{properties.disasterType}</p>
 				<p>{properties.description}</p>
 				<p>
@@ -22,7 +72,7 @@ export default function PopOver(props) {
 				<Button
 					variant="danger"
 					onClick={() => {
-						alert(properties.message);
+						respond(datas);
 					}}>
 					respond
 				</Button>
