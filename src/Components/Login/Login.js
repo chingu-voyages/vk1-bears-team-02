48dfb44bb35a84f51d9c9688f0fd801690c308d6
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-12-17 22:39:05 +0800

    change button to anchor tag in google button

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -10,102 +10,100 @@ import "./login.css";
 import backButton from "./img/back-button.svg";
 
 const Login = () => {
-  const { authenticated, setAuth } = useContext(AuthenticationContext);
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    if (data.username === "" || data.password === "") {
-      return swal({
-        text: "Please, fill out all form fields.",
-        icon: "warning",
-      });
-    }
-    console.log(data);
-    setAuth(true);
-  };
+	const { authenticated, setAuth } = useContext(AuthenticationContext);
+	const { register, handleSubmit } = useForm();
+	const onSubmit = (data) => {
+		if (data.username === "" || data.password === "") {
+			return swal({
+				text: "Please, fill out all form fields.",
+				icon: "warning",
+			});
+		}
+		console.log(data);
+		setAuth(true);
+	};
 
-  return (
-    <main className="page-container login-page">
-      <Nav page="Login" />
-      <Logo />
-      <Form
-        onSubmit={handleSubmit(onSubmit)}
-        className="d-flex flex-column align-items-center my-4"
-      >
-        <Form.Group controlId="formBasicEmail" className="w-50">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your username"
-            name="username"
-            className="form-field"
-            ref={register}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword" className="w-50">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            name="password"
-            className="form-field"
-            ref={register}
-          />
-        </Form.Group>
-        <div className="button-wrapper mt-3">
-          <button
-            className="btn btn-primary rounded-pill font-weight-bolder"
-            type="submit"
-          >
-            Login
-          </button>
-          <span className="or">or</span>
-          <button onClick={(e) => e.preventDefault()} class="google-btn">
-            Login with Google
-          </button>
-        </div>
-      </Form>
-      <Footer footerTitle="Don't have an account?" footerLink="Register" />
-    </main>
-  );
+	return (
+		<main className="page-container login-page">
+			<Nav page="Login" />
+			<Logo />
+			<Form
+				onSubmit={handleSubmit(onSubmit)}
+				className="d-flex flex-column align-items-center my-4">
+				<Form.Group controlId="formBasicEmail" className="w-50">
+					<Form.Label>Username</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Enter your username"
+						name="username"
+						className="form-field"
+						ref={register}
+					/>
+				</Form.Group>
+				<Form.Group controlId="formBasicPassword" className="w-50">
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type="password"
+						placeholder="Enter password"
+						name="password"
+						className="form-field"
+						ref={register}
+					/>
+				</Form.Group>
+				<div className="button-wrapper mt-3">
+					<button
+						className="btn btn-primary rounded-pill font-weight-bolder"
+						type="submit">
+						Login
+					</button>
+					<span className="or">or</span>
+					<a href="http://localhost:5000/google" class="google-btn">
+						Login with Google
+					</a>
+				</div>
+			</Form>
+			<Footer footerTitle="Don't have an account?" footerLink="Register" />
+		</main>
+	);
 };
 
 const Nav = ({ page }) => {
-  return (
-    <nav className="d-flex justify-content-between justify-content ">
-      <Link to="/">
-        <img
-          className="back-button mt-4 ml-4"
-          src={backButton}
-          alt="back button"
-        />
-      </Link>
-      <p className="page-name mt-4 mr-4">{page}</p>
-    </nav>
-  );
+	return (
+		<nav className="d-flex justify-content-between justify-content ">
+			<Link to="/">
+				<img
+					className="back-button mt-4 ml-4"
+					src={backButton}
+					alt="back button"
+				/>
+			</Link>
+			<p className="page-name mt-4 mr-4">{page}</p>
+		</nav>
+	);
 };
 
 const Logo = () => {
-  return (
-    <div className="container my-4">
-      <div className="row">
-        <div className="col-12 d-flex flex-column align-items-center">
-          <h1>e-Sagip</h1>
-          <p className="sub-title">One click away to get help.</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container my-4">
+			<div className="row">
+				<div className="col-12 d-flex flex-column align-items-center">
+					<h1>e-Sagip</h1>
+					<p className="sub-title">One click away to get help.</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const Footer = ({ footerTitle, footerLink }) => {
-  return (
-    <div className="footer d-flex flex-column align-items-center pb-4 mt-5">
-      <h6>{footerTitle}</h6>
-      <Link to={`/${footerLink}`} className="mt-n2 font-italic">
-        {footerLink}
-      </Link>
-    </div>
-  );
+	return (
+		<div className="footer d-flex flex-column align-items-center pb-4 mt-5">
+			<h6>{footerTitle}</h6>
+			<Link to={`/${footerLink}`} className="mt-n2 font-italic">
+				{footerLink}
+			</Link>
+		</div>
+	);
 };
 
 export { Login, Nav, Logo, Footer };
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -7,66 +7,67 @@ import "./register.css";
 import swal from "sweetalert";
 
 const Register = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    return swal({
-      text: "You are now registered!",
-      icon: "success",
-    });
-  };
-  return (
-    <main className="page-container register-page">
-      <Nav page="Register" />
-      <Logo />
-      <Form
-        onSubmit={handleSubmit(onSubmit)}
-        className="d-flex flex-column align-items-center my-4"
-      >
-        <Form.Group controlId="formBasicEmail" className="w-50">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your username"
-            name="username"
-            className="form-field"
-            ref={register({ required: true })}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail" className="w-50">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter your email"
-            name="email"
-            className="form-field"
-            ref={register({ required: true })}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword" className="w-50">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            name="password"
-            className="form-field"
-            ref={register({ required: true })}
-          />
-        </Form.Group>
+	const { register, handleSubmit } = useForm();
+	const onSubmit = (data) => {
+		return swal({
+			text: "You are now registered!",
+			icon: "success",
+		});
+	};
+	return (
+		<main className="page-container register-page">
+			<Nav page="Register" />
+			<Logo />
+			<Form
+				onSubmit={handleSubmit(onSubmit)}
+				className="d-flex flex-column align-items-center my-4">
+				<Form.Group controlId="formBasicEmail" className="w-50">
+					<Form.Label>Username</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Enter your username"
+						name="username"
+						className="form-field"
+						ref={register({ required: true })}
+					/>
+				</Form.Group>
+				<Form.Group controlId="formBasicEmail" className="w-50">
+					<Form.Label>Email</Form.Label>
+					<Form.Control
+						type="email"
+						placeholder="Enter your email"
+						name="email"
+						className="form-field"
+						ref={register({ required: true })}
+					/>
+				</Form.Group>
+				<Form.Group controlId="formBasicPassword" className="w-50">
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type="password"
+						placeholder="Enter password"
+						name="password"
+						className="form-field"
+						ref={register({ required: true })}
+					/>
+				</Form.Group>
 
-        <div className="button-wrapper mt-3">
-          <button
-            className="btn btn-primary rounded-pill font-weight-bolder"
-            type="submit"
-          >
-            Sign Up
-          </button>
-          <span className="or">or</span>
-          <button class="google-btn">Sign Up with Google</button>
-        </div>
-      </Form>
-      <Footer footerTitle="Already have an account?" footerLink="Login" />
-    </main>
-  );
+				<div className="button-wrapper mt-3">
+					<button
+						className="btn btn-primary rounded-pill font-weight-bolder"
+						type="submit">
+						Sign Up
+					</button>
+					<span className="or">or</span>
+
+					<a href="http://localhost:5000/google" class="google-btn">
+						Sign Up with Google
+					</a>
+				</div>
+			</Form>
+			<Footer footerTitle="Already have an account?" footerLink="Login" />
+		</main>
+	);
 };
 
 export default Register;
