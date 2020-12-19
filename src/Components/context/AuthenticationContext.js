f0d9d800bfdfd03693d7d4867eb2eae77395672e
mikotan-cpu <68640929+mikotan-cpu@users.noreply.github.com>
2020-12-19 23:54:04 +0800

    local storage, edit details

--- src/Components/context/AuthenticationContext.js
+++ src/Components/context/AuthenticationContext.js
@@ -4,8 +4,10 @@ export const AuthenticationContext = createContext(null);
 
 export const AuthenticationProvider = (props) => {
 	const [authenticated, setAuth] = useState(false);
+	const [details, setDetails] = useState(
+	)
 	return (
-		<AuthenticationContext.Provider value={{ authenticated, setAuth }}>
+		<AuthenticationContext.Provider value={{ authenticated, setAuth, details, setDetails }}>
 			{props.children}
 		</AuthenticationContext.Provider>
 	);
