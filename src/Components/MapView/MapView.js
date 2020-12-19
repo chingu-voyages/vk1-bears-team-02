a2ac60013af867d2cc118371e0347ef79448cdf6
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-12-19 20:24:21 +0800

    add real-time generation of map data

--- src/Components/MapView/MapView.js
+++ src/Components/MapView/MapView.js
@@ -5,6 +5,8 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
+import Pusher from "pusher-js";
+
 import axios from "axios";
 
 import "./styles.css";
@@ -12,7 +14,7 @@ import "./styles.css";
 import PopOver from "./PopOver";
 
 mapboxgl.accessToken =
-  "pk.eyJ1IjoiaXZhbmZ1bmNpb24iLCJhIjoiY2s5NGd5NGphMDFucjNpbDJ6d285cjNociJ9.HBWvHMgpYyY53znT78H8bA";
+	"pk.eyJ1IjoiaXZhbmZ1bmNpb24iLCJhIjoiY2s5NGd5NGphMDFucjNpbDJ6d285cjNociJ9.HBWvHMgpYyY53znT78H8bA";
 
 // mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
@@ -21,214 +23,229 @@ mapboxgl.accessToken =
 // 	// position: "absolute",
 // };
 const markerHeight = 50,
-  markerRadius = 10,
-  linearOffset = 25;
+	markerRadius = 10,
+	linearOffset = 25;
 
 const popupOffsets = {
-  top: [0, 0],
-  topLeft: [0, 0],
-  topRight: [0, 0],
-  bottom: [0, -markerHeight],
-  bottomLeft: [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
-  bottomRight: [
-    -linearOffset,
-    (markerHeight - markerRadius + linearOffset) * -1,
-  ],
-  left: [markerRadius, (markerHeight - markerRadius) * -1],
-  right: [-markerRadius, (markerHeight - markerRadius) * -1],
+	top: [0, 0],
+	topLeft: [0, 0],
+	topRight: [0, 0],
+	bottom: [0, -markerHeight],
+	bottomLeft: [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+	bottomRight: [
+		-linearOffset,
+		(markerHeight - markerRadius + linearOffset) * -1,
+	],
+	left: [markerRadius, (markerHeight - markerRadius) * -1],
+	right: [-markerRadius, (markerHeight - markerRadius) * -1],
 };
 
 const MapView = () => {
-  //example user
-  const [user, setUser] = useState("user");
+	//example user
+	const [user, setUser] = useState("user");
 
-  const [mapState, setMap] = useState(null);
+	const [mapState, setMap] = useState(null);
 
-  const [initialView, setInitialView] = useState({
-    lng: 120.9842,
-    lat: 14.5995,
-  });
+	const [initialView, setInitialView] = useState({
+		lng: 120.9842,
+		lat: 14.5995,
+	});
 
-  const [zoom, setZoom] = useState(5);
+	const [zoom, setZoom] = useState(5);
 
-  const [tiltAngle, setTiltAngle] = useState(1);
+	const [tiltAngle, setTiltAngle] = useState(1);
 
-  //  mapbox://styles/ivanfuncion/cki63927r0otl19t2eazqn8wq
-  const [mapstyle, setMapStyle] = useState("mapbox://styles/mapbox/dark-v10");
+	//  mapbox://styles/ivanfuncion/cki63927r0otl19t2eazqn8wq
+	const [mapstyle, setMapStyle] = useState("mapbox://styles/mapbox/dark-v10");
 
-  const [currentCoordinates, setCurrentCoordinates] = useState({
-    lng: 0,
-    lat: 0,
-  });
+	const [currentCoordinates, setCurrentCoordinates] = useState({
+		lng: 0,
+		lat: 0,
+	});
 
-  const mapContainer = useRef(null);
+	const mapContainer = useRef(null);
 
-  useEffect(() => {
-    const initializeMap = ({ setMap, mapContainer }) => {
-      const map = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: mapstyle,
-        center: [initialView.lng, initialView.lat],
-        zoom: zoom,
-        // pitch: tiltAngle,
-      });
+	useEffect(() => {
+		const initializeMap = ({ setMap, mapContainer }) => {
+			const map = new mapboxgl.Map({
+				container: mapContainer.current,
+				style: mapstyle,
+				center: [initialView.lng, initialView.lat],
+				zoom: zoom,
+				// pitch: tiltAngle,
+			});
 
-      if (user === "admin") {
-      } else {
-        // Initialize the geolocate control.
-        let geolocate = new mapboxgl.GeolocateControl({
-          positionOptions: {
-            enableHighAccuracy: true,
-          },
-          trackUserLocation: true,
-          showUserLocation: false,
-        });
-        // Add the control to the map.
-        map.addControl(geolocate, "bottom-left");
-        // geolocate.trigger(alert("trigger"));
+			if (user === "admin") {
+			} else {
+				// Initialize the geolocate control.
+				let geolocate = new mapboxgl.GeolocateControl({
+					positionOptions: {
+						enableHighAccuracy: true,
+					},
+					trackUserLocation: true,
+					showUserLocation: false,
+				});
+				// Add the control to the map.
+				map.addControl(geolocate, "bottom-left");
+				// geolocate.trigger(alert("trigger"));
 
-        // document.getElementById("trigger").addEventListener("click", () => {
-        // 	geolocate.trigger();
-        // });
-        const geocoder = new MapboxGeocoder({
-          accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
-          mapboxgl: mapboxgl,
-          color: "red",
-        });
-        console.log(geocoder);
-        map.addControl(geocoder);
+				// document.getElementById("trigger").addEventListener("click", () => {
+				// 	geolocate.trigger();
+				// });
+				const geocoder = new MapboxGeocoder({
+					accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+					mapboxgl: mapboxgl,
+					color: "red",
+				});
+				console.log(geocoder);
+				map.addControl(geocoder);
 
-        // get your current coordinates
-        geolocate.on("geolocate", (e) => {
-          var lng = e.coords.longitude;
-          var lat = e.coords.latitude;
-          var position = [lng, lat];
-          console.log(position);
+				// get your current coordinates
+				geolocate.on("geolocate", (e) => {
+					var lng = e.coords.longitude;
+					var lat = e.coords.latitude;
+					var position = [lng, lat];
+					console.log(position);
 
-          setCurrentCoordinates({
-            lng: lng,
-            lat: lat,
-          });
+					setCurrentCoordinates({
+						lng: lng,
+						lat: lat,
+					});
 
-          var iconHandler = document.createElement("div");
-          iconHandler.className = "marker-icon-current-coordinate-user-style";
+					var iconHandler = document.createElement("div");
+					iconHandler.className = "marker-icon-current-coordinate-user-style";
 
-          new mapboxgl.Marker(iconHandler).setLngLat([lng, lat]).addTo(map);
-        });
-      }
+					new mapboxgl.Marker(iconHandler).setLngLat([lng, lat]).addTo(map);
+				});
+			}
 
-      // add navigation control (zoom buttons)
-      let navigationControl = new mapboxgl.NavigationControl();
-      map.addControl(navigationControl, "bottom-right");
+			// add navigation control (zoom buttons)
+			let navigationControl = new mapboxgl.NavigationControl();
+			map.addControl(navigationControl, "bottom-right");
 
-      // add fullscreen control
-      // map.addControl(new mapboxgl.FullscreenControl());
+			// add fullscreen control
+			// map.addControl(new mapboxgl.FullscreenControl());
 
-      map.on("load", () => {
-        setMap(map);
+			map.on("load", () => {
+				setMap(map);
 
-        map.resize();
-      });
+				map.resize();
+			});
 
-      map.on("move", () => {
-        setInitialView({
-          lng: map.getCenter().lng.toFixed(4),
-          lat: map.getCenter().lat.toFixed(4),
-          // zoom: map.getZoom().toFixed(4),
-          // pitch: initialView.pitch,
-        });
+			map.on("move", () => {
+				setInitialView({
+					lng: map.getCenter().lng.toFixed(4),
+					lat: map.getCenter().lat.toFixed(4),
+					// zoom: map.getZoom().toFixed(4),
+					// pitch: initialView.pitch,
+				});
 
-        setZoom(map.getZoom().toFixed(4));
-      });
+				setZoom(map.getZoom().toFixed(4));
+			});
 
-      //not used
-      map.on("mouseleave", "water", function () {
-        console.log("A mouseleave event occurred.");
-      });
+			//not used
+			map.on("mouseleave", "water", function () {
+				console.log("A mouseleave event occurred.");
+			});
 
-      // setTimeout(() => {
-      // 	map.fitBounds([
-      // 		[32.958984, -5.353521],
-      // 		[43.50585, 5.615985],
-      // 	]);
-      // }, 5000);
+			// setTimeout(() => {
+			// 	map.fitBounds([
+			// 		[32.958984, -5.353521],
+			// 		[43.50585, 5.615985],
+			// 	]);
+			// }, 5000);
 
-      document.getElementById("bounce").addEventListener("click", () => {
-        map.fitBounds([
-          [32.958984, -5.353521],
-          [43.50585, 5.615985],
-        ]);
-      });
+			document.getElementById("bounce").addEventListener("click", () => {
+				map.fitBounds([
+					[32.958984, -5.353521],
+					[43.50585, 5.615985],
+				]);
+			});
 
-      const getAllCoordinates = async () => {
-        try {
-          console.log("call this");
-          const data = await axios.get(`http://localhost:5000/map-data`);
-          const features = data.data.features;
-          features.forEach((data) => {
-            console.log(data.geometry.coordinates);
+			// Enable pusher logging - don't include this in production
+			Pusher.logToConsole = true;
 
-            let popupHolder = document.createElement("div");
-            ReactDOM.render(
-              <PopOver feature="scooby" datas={data} />,
-              popupHolder
-            );
-            let popup = new mapboxgl.Popup({
-              offset: popupOffsets,
-              className: "popover-style",
-            })
-              .setLngLat(data.geometry.coordinates)
-              //.setHTML(<PopOver message="ivan" />)
-              // .setHTML(`<p>${data.properties.message}</p>`)
-              .setDOMContent(popupHolder);
-            // .setMaxWidth("320px")
-            // .addTo(map);
+			const pusher = new Pusher("b74a80c7be8fd2b220d7", {
+				cluster: "us3",
+			});
 
-            // create a HTML element for each feature
-            console.log(data.properties.disasterType);
-            var iconHandler = document.createElement("div");
-            if (data.properties.disasterType === "Flood") {
-              iconHandler.className = "marker-icon-flood-style";
-            } else if (data.properties.disasterType === "Fire") {
-              // alert("fire");
-              iconHandler.className = "marker-icon-fire-style";
-            } else if (data.properties.disasterType === "earthquake") {
-              iconHandler.className = "marker-icon-earthquake-style";
-            }
+			const getAllCoordinates = async () => {
+				try {
+					console.log("call this");
 
-            new mapboxgl.Marker(iconHandler)
-              .setLngLat(data.geometry.coordinates)
-              .addTo(map)
-              .setPopup(popup);
-          });
-        } catch (error) {
-          console.log(error);
-        }
-      };
+					const data = await axios.get(`http://localhost:5000/map-data`);
 
-      getAllCoordinates();
-      // console.log(`reinitialize`);
-    };
+					const features = data.data.features;
+					features.forEach((data) => {
+						console.log(data.geometry.coordinates);
 
-    if (!mapState) initializeMap({ setMap, mapContainer });
-  }, [mapState]);
+						let popupHolder = document.createElement("div");
+						ReactDOM.render(
+							<PopOver feature="scooby" datas={data} />,
+							popupHolder
+						);
+						let popup = new mapboxgl.Popup({
+							offset: popupOffsets,
+							className: "popover-style",
+						})
+							.setLngLat(data.geometry.coordinates)
+							//.setHTML(<PopOver message="ivan" />)
+							// .setHTML(`<p>${data.properties.message}</p>`)
+							.setDOMContent(popupHolder);
+						// .setMaxWidth("320px")
+						// .addTo(map);
 
-  return (
-    <div className="map-container">
-      <div
-        ref={(element) => (mapContainer.current = element)}
-        className="map-renderer"
-      />
-      <div className="sidebarStyle">
-        <p>
-          Longitude: {initialView.lng} | Latitude: {initialView.lat} | Zoom :{" "}
-          {zoom} | current: longitute {currentCoordinates.lng.toFixed(4)} and
-          latitude : {currentCoordinates.lat.toFixed(4)}
-        </p>
-        <button id="bounce">bounce</button>
+						// create a HTML element for each feature
+						console.log(data.properties.disasterType);
+						var iconHandler = document.createElement("div");
+						if (data.properties.disasterType === "Flood") {
+							iconHandler.className = "marker-icon-flood-style";
+						} else if (data.properties.disasterType === "Fire") {
+							// alert("fire");
+							iconHandler.className = "marker-icon-fire-style";
+						} else if (data.properties.disasterType === "earthquake") {
+							iconHandler.className = "marker-icon-earthquake-style";
+						}
 
-        <div>
-          {/* <select
+						new mapboxgl.Marker(iconHandler)
+							.setLngLat(data.geometry.coordinates)
+							.addTo(map)
+							.setPopup(popup);
+					});
+				} catch (error) {
+					console.log(error);
+				}
+			};
+
+			//init load
+			getAllCoordinates();
+
+			const channel = pusher.subscribe("map-data");
+			channel.bind("map-data-event", function (data) {
+				getAllCoordinates();
+			});
+			// console.log(`reinitialize`);
+		};
+
+		if (!mapState) initializeMap({ setMap, mapContainer });
+	}, [mapState]);
+
+	return (
+		<div className="map-container">
+			<div
+				ref={(element) => (mapContainer.current = element)}
+				className="map-renderer"
+			/>
+			<div className="sidebarStyle">
+				<p>
+					Longitude: {initialView.lng} | Latitude: {initialView.lat} | Zoom :{" "}
+					{zoom} | current: longitute {currentCoordinates.lng.toFixed(4)} and
+					latitude : {currentCoordinates.lat.toFixed(4)}
+				</p>
+				<button id="bounce">bounce</button>
+
+				<div>
+					{/* <select
 						onChange={(e) => {
 							setMapStyle(e.target.value);
 							console.log("yow ma man");
@@ -253,7 +270,7 @@ const MapView = () => {
 							Satellite street mode
 						</option>
 					</select> */}
-          {/* <button
+					{/* <button
 						onClick={() => {
 							setMapStyle("mapbox://styles/mapbox/dark-v10");
 						}}>
@@ -266,11 +283,11 @@ const MapView = () => {
 						Satellite street mode
 					</button> */}
 
-          {/* https://codepen.io/roblabs/pen/zJjPzX */}
-        </div>
-      </div>
-    </div>
-  );
+					{/* https://codepen.io/roblabs/pen/zJjPzX */}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MapView;
