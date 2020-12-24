868d5f293bc3c1d1c55bc997986fa2bb41aa01f8
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-12-24 17:56:21 +0800

    civilian able to send distress call to admin

--- src/Components/user/Earthquake/Earthquake.js
+++ src/Components/user/Earthquake/Earthquake.js
@@ -6,18 +6,18 @@ import Footer from "../Footer/Footer";
 import logo from "./img/earthquake.svg";
 
 function Earthquake() {
-  const earthquakeLogo = (
-    <img src={logo} width="60px" alt="earthquake logo" className="mb-4" />
-  );
-  return (
-    <main className="page-container">
-      <Nav />
-      <div className="help-button-container d-flex flex-column justify-content-center align-items-center">
-        <HelpButton logo={earthquakeLogo} />
-      </div>
-      <Footer />
-    </main>
-  );
+	const earthquakeLogo = (
+		<img src={logo} width="60px" alt="earthquake logo" className="mb-4" />
+	);
+	return (
+		<main className="page-container">
+			<Nav />
+			<div className="help-button-container d-flex flex-column justify-content-center align-items-center">
+				<HelpButton logo={earthquakeLogo} disasterType="Earthquake" />
+			</div>
+			<Footer />
+		</main>
+	);
 }
 
 export default Earthquake;
--- src/Components/user/HelpButton/HelpButton.js
+++ src/Components/user/HelpButton/HelpButton.js
@@ -1,15 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import swal from "sweetalert";
+
+import axios from "axios";
 
 import "./helpButton.css";
 
-const HelpButton = ({ logo }) => {
-	const handleClick = () => {
-		swal({
-			title: "Distress Sent fire",
-			text: "Your distress alert has been sent.",
-			icon: "success",
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Pusher from "pusher-js";
+
+const HelpButton = ({ logo, disasterType }) => {
+	const [currentUserCoordinate, setCurrentUserCoordinate] = useState({
+		lat: 0,
+		long: 0,
+	});
+
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.watchPosition(function (position) {
+				// console.log("Latitude is :", position.coords.latitude);
+				// console.log("Longitude is :", position.coords.longitude);
+
+				setCurrentUserCoordinate({
+					lat: position.coords.latitude,
+					long: position.coords.longitude,
+				});
+			});
+		}
+
+		Pusher.logToConsole = true;
+
+		const pusher = new Pusher("b74a80c7be8fd2b220d7", {
+			cluster: "us3",
 		});
+
+		const channel2 = pusher.subscribe("map-data-update");
+		channel2.bind("map-data-update-event", function (data) {
+			if (data.data.status === "acknowledge") {
+				toast.info(`Update status to ${data.data.status}`);
+			} else {
+				toast.success(`Update status to ${data.data.status}`);
+			}
+		});
+	}, []);
+
+	const handleClick = () => {
+		// swal({
+		// 	title: `Distress Sent ${disasterType}`,
+		// 	text: `Your distress alert has been sent. ${currentUserCoordinate.lat} ${currentUserCoordinate.long}`,
+		// 	icon: "success",
+		// });
+
+		const sentDistressCall = async (req, res) => {
+			try {
+				const data = {
+					civilian: `5fcf8dc19e22ce1544064770`,
+					longitude: currentUserCoordinate.long,
+					latitude: currentUserCoordinate.lat,
+					description: "sdadadadasd",
+					title: "sdadadadasd",
+					disasterType: disasterType,
+				};
+
+				const response = await axios.post(
+					"http://localhost:5000/map-data",
+					data
+				);
+				// console.log(response);
+				if (response.statusText === "OK") {
+					toast.error(`Distress Call has been sent`);
+				}
+			} catch (error) {
+				console.error(`error:${error}`);
+			}
+		};
+
+		sentDistressCall();
 	};
 	return (
 		<>
@@ -20,6 +87,7 @@ const HelpButton = ({ logo }) => {
 				</div>
 				<div class="help-button-outline"></div>
 			</div>
+			<ToastContainer />
 		</>
 	);
 };
