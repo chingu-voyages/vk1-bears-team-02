b7fbc80bd9711c0d9e97b3daa0272f962b6e64c6
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-01-05 17:28:07 +0800

    update admin login validation

--- src/Components/admin/Login/Login.js
+++ src/Components/admin/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Nav, Logo } from "../../Login/Login";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
@@ -13,7 +13,24 @@ import "react-toastify/dist/ReactToastify.css";
 import { AuthenticationContext } from "../../context/AuthenticationContext";
 function Login() {
 	const { authenticated, setAuth } = useContext(AuthenticationContext);
-	const { register, handleSubmit } = useForm();
+	const { register, handleSubmit, errors } = useForm();
+
+	const [disableField, setDisableField] = useState(false);
+
+	const [userData, setUserData] = useState({
+		username: "",
+		password: "",
+	});
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		setUserData({
+			...userData,
+			[e.target.name]: value,
+		});
+
+		// console.log(userData);
+	};
 
 	useEffect(() => {
 		// alert(window.history.back());
@@ -21,52 +38,44 @@ function Login() {
 	}, []);
 
 	const onSubmit = (data) => {
-		console.log(data);
-		if (data.username === "" || data.password === "") {
-			// return swal({
-			// 	text: "Please, fill out all form fields.",
-			// 	icon: "warning",
-			// });
-			toast.warning("Please, fill out all form fields!");
-		} else {
-			const getLoginDataWithForm = async (req, res) => {
-				try {
-					const response = await axios.post(
-						`${process.env.REACT_APP_BACKEND}admin/login`,
-						{
-							username: data.username,
-							password: data.password,
-						}
-					);
-					console.log(response);
-
-					const message = response.data.message;
-
-					if (message === "user login") {
-						toast.success(`${message}`, { autoClose: 2000 });
-						const token = response.data.token;
-						const decoded = jwt_decode(token);
-
-						localStorage.setItem("role", decoded.role);
-						localStorage.setItem("username", decoded.username);
-						localStorage.setItem("email", decoded.email);
-						localStorage.setItem("userId", decoded.userId);
-						localStorage.setItem("setAuth", true);
-						console.log(decoded);
-						setTimeout(function () {
-							window.location.replace(`${process.env.REACT_APP_FRONTEND}admin`);
-							// setAuth(true);
-						}, 3000);
-					} else {
-						toast.warning(`${message}`, { autoClose: 2000 });
+		setDisableField(true);
+		const getLoginDataWithForm = async (req, res) => {
+			try {
+				const response = await axios.post(
+					`${process.env.REACT_APP_BACKEND}admin/login`,
+					{
+						username: data.username,
+						password: data.password,
 					}
-				} catch (error) {
-					console.error(`error:${error}`);
-				}
-			};
+				);
+				console.log(response);
 
-			getLoginDataWithForm();
-		}
+				const message = response.data.message;
+
+				if (message === "user login") {
+					toast.success(`${message}`, { autoClose: 2000 });
+					const token = response.data.token;
+					const decoded = jwt_decode(token);
+
+					localStorage.setItem("role", decoded.role);
+					localStorage.setItem("username", decoded.username);
+					localStorage.setItem("email", decoded.email);
+					localStorage.setItem("userId", decoded.userId);
+					localStorage.setItem("setAuth", true);
+					console.log(decoded);
+					setTimeout(function () {
+						window.location.replace(`${process.env.REACT_APP_FRONTEND}admin`);
+						// setAuth(true);
+					}, 3000);
+				} else {
+					toast.warning(`${message}`, { autoClose: 2000 });
+				}
+			} catch (error) {
+				console.error(`error:${error}`);
+			}
+		};
+
+		getLoginDataWithForm();
 	};
 	return (
 		<main className="page-container login-page">
@@ -82,8 +91,15 @@ function Login() {
 						placeholder="Enter your username"
 						name="username"
 						className="form-field"
-						ref={register}
+						onChange={handleChange}
+						ref={register({ required: true })}
+						disabled={disableField}
 					/>
+					{errors.username && (
+						<span>
+							<p className="error-two">Username is required</p>
+						</span>
+					)}
 				</Form.Group>
 				<Form.Group controlId="formBasicPassword" className="w-50">
 					<Form.Label>Password</Form.Label>
@@ -92,8 +108,15 @@ function Login() {
 						placeholder="Enter password"
 						name="password"
 						className="form-field"
-						ref={register}
+						onChange={handleChange}
+						ref={register({ required: true })}
+						disabled={disableField}
 					/>
+					{errors.password && (
+						<span>
+							<p className="error-two">Password is required</p>
+						</span>
+					)}
 				</Form.Group>
 				<div className="button-wrapper mt-3">
 					<button
