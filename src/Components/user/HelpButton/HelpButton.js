5d3a557eaa41d3644fdeafddde46783832ed14ee
John Carlo Cunanan <codingjohn@outlook.com>
2021-01-06 20:11:45 +0800

    change toast color to green

--- src/Components/user/HelpButton/HelpButton.js
+++ src/Components/user/HelpButton/HelpButton.js
@@ -11,85 +11,85 @@ import "react-toastify/dist/ReactToastify.css";
 import Pusher from "pusher-js";
 
 const HelpButton = ({ logo, disasterType }) => {
-	const [currentUserCoordinate, setCurrentUserCoordinate] = useState({
-		lat: 0,
-		long: 0,
-	});
+  const [currentUserCoordinate, setCurrentUserCoordinate] = useState({
+    lat: 0,
+    long: 0,
+  });
 
-	useEffect(() => {
-		if (navigator.geolocation) {
-			navigator.geolocation.watchPosition(function (position) {
-				// console.log("Latitude is :", position.coords.latitude);
-				// console.log("Longitude is :", position.coords.longitude);
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(function (position) {
+        // console.log("Latitude is :", position.coords.latitude);
+        // console.log("Longitude is :", position.coords.longitude);
 
-				setCurrentUserCoordinate({
-					lat: position.coords.latitude,
-					long: position.coords.longitude,
-				});
-			});
-		}
+        setCurrentUserCoordinate({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+      });
+    }
 
-		Pusher.logToConsole = true;
+    Pusher.logToConsole = true;
 
-		const pusher = new Pusher(process.env.REACT_APP_PUSHER_ID, {
-			cluster: "us3",
-		});
+    const pusher = new Pusher(process.env.REACT_APP_PUSHER_ID, {
+      cluster: "us3",
+    });
 
-		const channel2 = pusher.subscribe("map-data-update");
-		channel2.bind("map-data-update-event", function (data) {
-			if (data.data.status === "acknowledge") {
-				toast.info(`Update status to ${data.data.status}`);
-			} else {
-				toast.success(`Update status to ${data.data.status}`);
-			}
-		});
-	}, []);
+    const channel2 = pusher.subscribe("map-data-update");
+    channel2.bind("map-data-update-event", function (data) {
+      if (data.data.status === "acknowledge") {
+        toast.info(`Update status to ${data.data.status}`);
+      } else {
+        toast.success(`Update status to ${data.data.status}`);
+      }
+    });
+  }, []);
 
-	const handleClick = () => {
-		// swal({
-		// 	title: `Distress Sent ${disasterType}`,
-		// 	text: `Your distress alert has been sent. ${currentUserCoordinate.lat} ${currentUserCoordinate.long}`,
-		// 	icon: "success",
-		// });
+  const handleClick = () => {
+    // swal({
+    // 	title: `Distress Sent ${disasterType}`,
+    // 	text: `Your distress alert has been sent. ${currentUserCoordinate.lat} ${currentUserCoordinate.long}`,
+    // 	icon: "success",
+    // });
 
-		const sentDistressCall = async (req, res) => {
-			try {
-				const data = {
-					civilian: localStorage.getItem("userId"),
-					longitude: currentUserCoordinate.long,
-					latitude: currentUserCoordinate.lat,
-					// description: "sdadadadasd",
-					// title: "sdadadadasd",
-					disasterType: disasterType,
-				};
+    const sentDistressCall = async (req, res) => {
+      try {
+        const data = {
+          civilian: localStorage.getItem("userId"),
+          longitude: currentUserCoordinate.long,
+          latitude: currentUserCoordinate.lat,
+          // description: "sdadadadasd",
+          // title: "sdadadadasd",
+          disasterType: disasterType,
+        };
 
-				const response = await axios.post(
-					`${process.env.REACT_APP_BACKEND}map-data`,
-					data
-				);
-				// console.log(response);
-				if (response.statusText === "OK") {
-					toast.error(`Distress Call has been sent`);
-				}
-			} catch (error) {
-				console.error(`error:${error}`);
-			}
-		};
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND}map-data`,
+          data
+        );
+        // console.log(response);
+        if (response.statusText === "OK") {
+          toast.success(`Distress Call has been sent`);
+        }
+      } catch (error) {
+        console.error(`error:${error}`);
+      }
+    };
 
-		sentDistressCall();
-	};
-	return (
-		<>
-			<div className="help-button-wrapper" onClick={handleClick}>
-				<div className="help-button d-flex justify-content-center align-items-center flex-column">
-					{logo}
-					<h2>Click to get help!</h2>
-				</div>
-				<div class="help-button-outline"></div>
-			</div>
-			<ToastContainer />
-		</>
-	);
+    sentDistressCall();
+  };
+  return (
+    <>
+      <div className="help-button-wrapper" onClick={handleClick}>
+        <div className="help-button d-flex justify-content-center align-items-center flex-column">
+          {logo}
+          <h2>Click to get help!</h2>
+        </div>
+        <div class="help-button-outline"></div>
+      </div>
+      <ToastContainer />
+    </>
+  );
 };
 
 export default HelpButton;
